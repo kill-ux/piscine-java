b3913f0763d50c7304667dd3dc5b3e55f68d307c
Mustapha Boutoub | killux <mustaphaboutoubdev@gmail.com>
2025-08-11 14:38:17 +0100

    Palindrome/Palindrome.java

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
+        if (array == null) {
+            return null;
+        }
         return Arrays.stream(array).map(ele -> {
             if (ele % 3 == 0) {
                 return ele * 5;
