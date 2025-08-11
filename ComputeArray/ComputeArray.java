abcae7e10da5f50e3c91a9ed086b7b8cb37e5d43
Mustapha Boutoub | killux <mustaphaboutoubdev@gmail.com>
2025-08-11 14:35:46 +0100

    Palindrome/Palindrome.java

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -6,7 +6,7 @@ public class ComputeArray {
             if (ele % 3 == 0) {
                 return ele * 5;
             } else if (ele % 3 == 1) {
-                return ele + 7;
+                return ele + (ele > 0 ? 7 : -7);
             } else {
                 return ele;
             }
