3b7419482f3497aff426de447b4f30e28ca47849
Mustapha Boutoub | killux <mustaphaboutoubdev@gmail.com>
2025-08-11 12:20:26 +0100

    StringLength/StringLength.java

--- StringLength/ExerciseRunner.java
+++
@@ -1,6 +0,0 @@
-public class ExerciseRunner {
-    public static void main(String[] args) {
-        System.out.println(StringLength.getStringLength("Hello World !"));
-        System.out.println(StringLength.getStringLength(""));
-    }
-}
