b6e723e9344f2c5365902f72ada032c98f907735
Mustapha Boutoub | killux <mustaphaboutoubdev@gmail.com>
2025-08-11 14:12:18 +0100

    Palindrome/Palindrome.java

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,12 +1,12 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
         if (s.length() % 2 == 0) {
-            String s1 = s.substring(0, s.length() / 2);
-            String s2 = new StringBuffer(s.substring(s.length() / 2)).reverse().toString();
+            String s1 = s.substring(0, s.length() / 2).toLowerCase();
+            String s2 = new StringBuffer(s.substring(s.length() / 2)).reverse().toString().toLowerCase();
             return s1 == s2;
         } else {
-            String s1 = s.substring(0, s.length() / 2);
-            String s2 = new StringBuffer(s.substring((s.length() / 2) + 1)).reverse().toString();
+            String s1 = s.substring(0, s.length() / 2).toLowerCase();
+            String s2 = new StringBuffer(s.substring((s.length() / 2) + 1)).reverse().toString().toLowerCase();
             return s1.equals(s2);
         }
     }
