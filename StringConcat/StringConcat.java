9afc68f5eea521e5328816a70aa238563d1e20eb
Mustapha Boutoub | killux <mustaphaboutoubdev@gmail.com>
2025-08-11 12:25:22 +0100

    StringLength/StringLength.java

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,8 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return null;
+        }
         String str1 = s1 == null ? "" : s1;
         String str2 = s2 == null ? "" : s2;
         return str1 + str2;
