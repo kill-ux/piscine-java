aa6a88474ee61432aa42695e4e56999f210b2608
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-12 10:26:47 +0100

    SortAdd

--- SortArgs/SortArgs.java
+++ SortArgs/SortArgs.java
@@ -1,14 +1,17 @@
+import java.util.Arrays;
+
 public class SortArgs {
     public static void sort(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            for (int j = 0; j < args.length; j++) {
-                if (args[i].compareTo(args[j]) < 0) {
-                    String temp = args[i];
-                    args[i] = args[j];
-                    args[j] = temp;
-                }
-            }
-        }
+        // for (int i = 0; i < args.length; i++) {
+        //     for (int j = 0; j < args.length; j++) {
+        //         if (args[i].compareTo(args[j]) < 0) {
+        //             String temp = args[i];
+        //             args[i] = args[j];
+        //             args[j] = temp;
+        //         }
+        //     }
+        // }
+        Arrays.sort(args);
         System.out.println(String.join(" ", args));
     }
 }
