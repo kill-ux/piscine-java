bc0fc5f6226eb2d22e9f1812b06a44a1ceae0f95
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-12 10:25:14 +0100

    SortAdd

--- DoOp/ExerciseRunner.java
+++
@@ -1,8 +0,0 @@
-public class ExerciseRunner {
-    public static void main(String[] args) {
-        System.out.println(DoOp.operate(new String[]{"1","+","2"}));
-        System.out.println(DoOp.operate(new String[]{"1","-","1"}));
-        System.out.println(DoOp.operate(new String[]{"1","%","1"}));
-        System.out.println(DoOp.operate(args));
-    }
-}
---
+++ SortArgs/SortArgs.java
@@ -0,0 +1,14 @@
+public class SortArgs {
+    public static void sort(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            for (int j = 0; j < args.length; j++) {
+                if (args[i].compareTo(args[j]) < 0) {
+                    String temp = args[i];
+                    args[i] = args[j];
+                    args[j] = temp;
+                }
+            }
+        }
+        System.out.println(String.join(" ", args));
+    }
+}
