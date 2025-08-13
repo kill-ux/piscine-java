358016f93e1b81fd923baca6b21cbee7cea3b2f3
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-13 12:19:52 +0100

    o

---
+++ StarInheritance/Star.java
@@ -0,0 +1,11 @@
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+
+    public double setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+}
