8db10ade60b09005a42f8d91ac477d4cc6e20dd1
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-13 13:23:24 +0100

    start with overides

---
+++ StarGalaxy/Planet.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Planet extends CelestialObject {
+    private Star centerStar;
+
+    public Planet() {
+        super();
+        this.centerStar = new Star();
+    }
+
+    public Planet(String name, double x, double y, double z, Star centerStar) {
+        super(name, x, y, z);
+        this.centerStar = centerStar;
+    }
+
+    public Star getCenterStar() {
+        return centerStar;
+    }
+
+    public void setCenterStar(Star centerStar) {
+        this.centerStar = centerStar;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), centerStar.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Planet) {
+            Planet planet = (Planet) object;
+            return super.equals(planet) && this.centerStar.equals(planet.getCenterStar());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s circles around %s at the %.3f AU", this.getName(), this.centerStar.getName(),
+                this.getDistanceBetween(this.centerStar, this));
+    }
+
+}
