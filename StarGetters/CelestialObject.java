84b30feda563bc3d2379ee97e88e4619335c8756
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-13 10:28:39 +0100

    constructers

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,50 @@
+
+public class CelestialObject {
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+
+    public CelestialObject() {
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setX(double value) {
+        this.x = value;
+    }
+
+    public void setY(double value) {
+        this.y = value;
+    }
+
+    public void setZ(double value) {
+        this.z = value;
+    }
+
+    public void setName(String value) {
+        this.name = value;
+    }
+}
