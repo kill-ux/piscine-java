12dafb1a93555fda88bb6a4f6be497370558949e
killux <mustaphaboutoubdev@gmail.com>
2025-08-18 11:29:55 +0100

    Character

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,42 @@
+public class Character {
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.currentHealth = maxHealth;
+        this.maxHealth = maxHealth;
+    }
+
+    @Override
+    public String toString() {
+        if (currentHealth == 0) {
+            return String.format("%s : KO", this.name);
+        }
+        return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void takeDamage(int subtract) {
+        if (this.currentHealth - subtract > 0) {
+            this.currentHealth -= subtract;
+        } else {
+            this.currentHealth = 0;
+        }
+    }
+    public void attack(Character character) {
+        takeDamage(9);
+    }
+}
