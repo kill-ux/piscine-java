2b37e1dba9d58b81ecc042b4cce2dbbff1060e64
killux <mustaphaboutoubdev@gmail.com>
2025-08-18 15:28:06 +0100

    s

--- AdventureWeapon/ExerciseRunner.java
+++ AdventureWeapon/ExerciseRunner.java
@@ -4,19 +4,12 @@ public class ExerciseRunner {
         Weapon narsil = new Weapon("Narsil", 15);
         Weapon baguette = new Weapon("Baguette magique", 20);
         Weapon massue = new Weapon("Massue", 8);
-        Monster troll = new Monster("Troll", 30, massue);
+        Monster troll = new Monster("Troll", 30,  massue);
         Sorcerer dumbledore = new Sorcerer("Dumbledore", 25, 5, baguette);
         Templar alistair = new Templar("Alistair", 18, 2, 3, narsil);
 
-        Weapon q = new Weapon("Baguette", 16);
-        Sorcerer dragoMalefoy = new Sorcerer("Drago Malefoy", 20, 2, null);
-        Sorcerer harryPotter = new Sorcerer("Harry Potter", 30, 4, q);
+        Character.fight(alistair, troll);
 
-        harryPotter.attack(dragoMalefoy);
-        System.out.println(dragoMalefoy.getCurrentHealth());
-
-        // Character.fight(alistair, troll);
-
-        // System.out.println(Character.printStatus());
+        System.out.println(Character.printStatus());
     }
 }
