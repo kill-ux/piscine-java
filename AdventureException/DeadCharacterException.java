f26fac2766ca839f83d287306e227000b59fbf76
killux <mustaphaboutoubdev@gmail.com>
2025-08-18 16:02:59 +0100

    s

--- AdventureException/DeadCharacterException.java
+++ AdventureException/DeadCharacterException.java
@@ -1,13 +1,12 @@
 public class DeadCharacterException extends Exception {
     private Character character;
 
-    DeadCharacterException(Character ch) {
+    public DeadCharacterException(Character ch) {
         ch.setCurrentHealth(0);
         this.character = ch;
     }
 
     public String getMessage() {
-        System.out.println("[DEBUG] Class name: " + character.getClass().getSimpleName().toLowerCase());
         return String.format("The %s %s is dead.", character.getClass().getSimpleName().toLowerCase(), character.getName());
     }
 }
--- AdventureException/ExerciseRunner.java
+++ AdventureException/ExerciseRunner.java
@@ -1,12 +1,15 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class ExerciseRunner {
 
     public static void main(String[] args) {
         Weapon feu = new Weapon("Boule de feu", 7);
-        Sorcerer triss = new Sorcerer("Triss Merigold", 30, 5, feu); 
-        Templar geralt = new Templar("Geralt de Riv", 28, 2, 5, feu); 
+        Sorcerer triss = new Sorcerer("Triss Merigold", 30, 5, feu);
+        Templar geralt = new Templar("Geralt de Riv", 28, 2, 5, feu);
         DeadCharacterException exceptionTriss = new DeadCharacterException(triss);
         DeadCharacterException exceptionGeralt = new DeadCharacterException(geralt);
-        
+
         System.out.println(exceptionTriss.getMessage());
         System.out.println(exceptionGeralt.getMessage());
     }
