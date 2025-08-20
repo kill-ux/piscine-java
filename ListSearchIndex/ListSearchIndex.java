098e35fa4ccc4d94e86a7b7e96eef78f7dbaa15d
killux <mustaphaboutoubdev@gmail.com>
2025-08-20 10:43:10 +0100

    search

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -3,11 +3,11 @@ import java.util.List;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
-        int index = list.indexOf(value);
+        int index = list.lastIndexOf(value);
         return index == -1 ? null : index;
     }
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
-        int index = list.lastIndexOf(value);
+        int index = list.indexOf(value);
         return index == -1 ? null : index;
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
