8039ede321e6657a87ebb572cfd14ac7a25aced6
killux <mustaphaboutoubdev@gmail.com>
2025-08-20 10:59:29 +0100

    sort

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -1,15 +1,20 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
-        list.sort(null);
-        return list;
+        if (list == null) return null;
+        List<Integer> l = new ArrayList<>(list);
+        l.sort(null);
+        return l;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
-        list.sort((x, y) -> y.compareTo(y));
-        return list;
+        if (list == null) return null;
+        List<Integer> l = new ArrayList<>(list);
+        l.sort((x, y) -> y.compareTo(y));
+        return l;
     }
 }
