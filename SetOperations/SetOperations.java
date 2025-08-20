5d6281249105569da8e82621e4f9467a42edc9ee
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-20 11:45:43 +0100

    containes

---
+++ SetOperations/SetOperations.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        set1.addAll(set2);
+        return set1;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        set1.retainAll(set2);
+        return set1;
+    }
+}
