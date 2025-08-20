e44ef6ff49e8ba0e86ea069c659a54e295a0ca7b
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-20 17:13:09 +0100

    readuuce

---
+++ WeddingComplex/WeddingComplex.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.Map;
+
+public class WeddingComplex {
+    public static Map<String, String> createBestCouple(Map<String, List<String>> first, Map<String, List<String>> second) {
+        // your code here
+    }
+}
