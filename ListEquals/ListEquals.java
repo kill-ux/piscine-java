e1690b677ad2baddfffe2841b59a0eed88f1bdd8
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-20 10:25:22 +0100

    containes

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,7 +2,8 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1 == null ^ list2 == null) return false;
+        if (list1 == null && list2 == null) return true;
+        if (list1 == null ^ list2 == null) return true;
         return list1.equals(list2);
     }
 }
