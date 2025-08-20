39ee2ee1c0cbaea43cc51d71c4aebe60fe71d580
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-20 12:44:37 +0100

    containes

---
+++ StreamCollect/StreamCollect.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class StreamCollect {
+    public static Map<Character, List<String>> mapByFirstLetter(Stream<String> s) {
+        Map<Character, List<String>> map = new HashMap<>();
+        s.forEach((w) -> {
+            char ch = w.toUpperCase().charAt(0);
+            if (!map.containsKey(ch)) {
+                map.put(w.toUpperCase().charAt(0), new ArrayList<>());
+            }
+            map.get(ch).add(w);
+        });
+        return map;
+    }
+
+    public static Map<Integer, Optional<Integer>> getMaxByModulo4(Stream<Integer> s) {
+        Map<Integer, Optional<Integer>> map = new HashMap<>();
+        map.put(0, Optional.empty());
+        map.put(1, Optional.empty());
+        map.put(2, Optional.empty());
+        map.put(3, Optional.empty());
+        s.forEach((num) -> {
+            Optional<Integer> unwrap = map.get(num % 4);
+            if (unwrap.isPresent()) {
+                map.put(map.get(num % 4).get(), Optional.of(unwrap.get() > num ? unwrap.get() : num));
+            }
+        });
+        return map;
+    }
+
+    public static String orderAndConcatWithSharp(Stream<String> s) {
+        return String.format("{%s}", String.join("#", s.sorted().toList()));
+    }
+}
