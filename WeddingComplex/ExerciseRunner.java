1e46ae38cb9814fac364c644b07f638ed93ba632
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-21 12:04:18 +0100

    readuuce

--- WeddingComplex/ExerciseRunner.java
+++ WeddingComplex/ExerciseRunner.java
@@ -5,10 +5,7 @@ public class ExerciseRunner {
 
     public static void main(String[] args) {
         System.out.println(WeddingComplex.createBestCouple(
-                // Map.of("Naruto", List.of("Sakura", "Hinata"), "Sasuke", List.of("Sakura", "Hinata")),
-                // Map.of("Sakura", List.of("Sasuke", "Naruto"), "Hinata", List.of("Naruto", "Sasuke"))));
-
-                Map.of("A", List.of("X", "Y" , "Z"), "B", List.of("Y" , "X" , "Z") , "C", List.of("Y" , "Z" , "X")),
-                Map.of("X", List.of("B", "A" , "C"), "Y", List.of("A" , "C" , "B") , "Z", List.of("B" , "C" , "A"))));
+                Map.of("Naruto", List.of("Sakura", "Hinata"), "Sasuke", List.of("Sakura", "Hinata")),
+                Map.of("Sakura", List.of("Sasuke", "Naruto"), "Hinata", List.of("Naruto", "Sasuke"))));
     };
 }
