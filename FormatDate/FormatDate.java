a85bf804de3877e593a927c755b0e3e6215fb424
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-25 14:20:01 +0100

    time

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -7,15 +7,18 @@ import java.util.Locale;
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
+        if (dateTime == null) return null;
         return dateTime.format(
                 DateTimeFormatter.ofPattern("'Le' dd MMM 'de l''an' yyyy 'à' HH'h'mm'm' 'et' ss's'", Locale.FRENCH));
     }
 
     public static String formatSimple(LocalDate date) {
+        if (date == null) return null;
         return date.format(DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALY));
     }
 
     public static String formatIso(LocalTime time) {
+        if (time == null) return null;
         return time.toString();
     }
 
