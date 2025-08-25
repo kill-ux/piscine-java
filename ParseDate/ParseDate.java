0287302cf35ff83fd3ec2d266922edc9f2d200f4
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-25 13:02:51 +0100

    time

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -8,14 +8,22 @@ import java.util.Locale;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if (stringDate == null)
+            return null;
         return LocalDateTime.parse(stringDate);
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (stringDate == null)
+            return null;
+
         return LocalDate.parse(stringDate, DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy", Locale.FRENCH));
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
+        if (stringDate == null)
+            return null;
+
         LocalTime time = LocalTime.parse(stringDate,
                 DateTimeFormatter.ofPattern("HH 'heures du soir,' mm 'minutes et' ss 'secondes'"));
         if (stringDate.contains("heures du soir")) {
