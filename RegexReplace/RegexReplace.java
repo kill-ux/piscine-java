14d3f48850935b8de56743b4e558f638b23cc678
kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-25 18:11:36 +0100

    time

--- RegexReplace/RegexReplace.java
+++ RegexReplace/RegexReplace.java
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern;
+
 public class RegexReplace {
     public static String removeUnits(String s) {
         return s.replaceAll("(?<=\\d+)(cm|€)(?= |$)", "");
@@ -7,12 +9,17 @@ public class RegexReplace {
         String[] array = s.split("@");
         String user = "";
         String first = array[0];
+        String levels = array[1];
         if (first.length() > 3) {
-            user =  first.substring(0, Math.max(0, first.length() - 3)) + "*".repeat(Math.min(3, first.length()));
+            user = first.substring(0, Math.max(0, first.length() - 3)) + "*".repeat(Math.min(3, first.length()));
         } else {
             user = array[0].replaceAll("(?<=[-|.|_]).", "*");
         }
-        String secend = array[1].replaceAll("((.)(?=.*\\..*\\..*))|((?<=.*\\..*\\..*)(.))","*");
-        return user + "@" + secend;
+        if (levels.matches(".*\\..*\\..*$")) {
+            levels = array[1].replaceAll("((.)(?=.*\\..*\\..*))|((?<=.*\\..*\\..*)(.))", "*");
+        } else {
+            System.out.println("kk");
+        }
+        return user + "@" + levels;
     }
 }
